c840286b5b1af818da9295ac2d1941a8e7b4f754
Aleksey Osipov <alexey11rus@gmail.com>
2020-04-29 23:09:24 +0300

    feat: slides load from data

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -2,6 +2,36 @@ import slides from './modules/slides';
 import filmsData from './modules/data';
 
 
-// filmsData.Search.forEach()
+const swiperWrapper = document.querySelector('.swiper-wrapper');
+
+const loadCards = () => {
+  filmsData.Search.forEach((film)=>{
+    swiperWrapper.innerHTML +=  
+    `
+    <div class="swiper-slide">
+    <div class="movie-slide">
+      <div class="movie-picture">
+        <img src="${film.Poster}">
+      </div>
+      <div class="movie-description">
+        <div class="movie-name">
+          <h3>${film.Title}</h3>
+        </div>
+        <div class="movie-year">
+          <h4>${film.Year}</h4>
+          <span class="imdb">${film.imdbRating}</span>
+        </div>
+      </div>
+    </div>
+    </div>
+    `
+  });
+}
+
+
+loadCards();
+
+slides.update();
+
 
 
